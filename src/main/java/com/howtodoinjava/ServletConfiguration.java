eb28b75e1a0a757509c7a4ed57570fd226b94eda
kurczews <krzysiek.kurczewski@gmail.com>
2022-09-09 19:04:06 +0200

    Use Tomcat as default

--- src/main/java/com/howtodoinjava/ServletConfiguration.java
+++ src/main/java/com/howtodoinjava/ServletConfiguration.java
@@ -1,7 +1,6 @@
 package com.howtodoinjava;
 
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +8,6 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class ServletConfiguration {
-
-    @Bean
-    @Profile("tomcat")
-    public ServletWebServerFactory tomcatServlet() {
-        return new TomcatServletWebServerFactory();
-    }
 
     @Bean
     @Profile("jetty")
