b97467186913f497b55d160de2bc3a45a8f4b2ec
kurczews <krzysiek.kurczewski@gmail.com>
2022-09-09 16:47:27 +0200

    Working Tomcat

---
+++ src/main/java/com/howtodoinjava/app/controller/MvcConfiguration.java
@@ -0,0 +1,18 @@
+package com.howtodoinjava.app.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+public class MvcConfiguration {
+//    @Bean
+    public InternalResourceViewResolver configureViewResolvers() {
+        var resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/view/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+}
